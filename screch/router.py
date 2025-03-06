c9c09032502efaaac6a9ff294156ab68f7406588
Yegor <egorp563@gmail.com>
2025-03-06 17:37:36 +0300

    Добавил комментарии в router.py

--- screch/router.py
+++ screch/router.py
@@ -4,8 +4,11 @@ from screch.question_handler import router as question_router
 from screch.answer_handler import router as answer_router
 from keyboards.button_handlers.confirm_start import router as confirm_start_router
 
+# Основной роутер модуля screch
 router = Router()
-router.include_router(start_router)
-router.include_router(question_router)
-router.include_router(answer_router)
-router.include_router(confirm_start_router)
+
+# Подключаем вспомогательные роутеры:
+router.include_router(start_router)           # для старта опроса
+router.include_router(question_router)        # - для обработки вопросов
+router.include_router(answer_router)          # - для обработки ответов
+router.include_router(confirm_start_router)   # для подтверждения начала
